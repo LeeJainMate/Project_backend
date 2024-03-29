834ac6a00798ba50ac15d49ecbd9ef7c2d0960ab
LeeJainMate <leejainmate@gmail.com>
2024-03-29 07:08:22 +0700

    Update login.ts

--- api/login.ts
+++ api/login.ts
@@ -1,16 +1,17 @@
 import express from "express";
 import { conn } from "../dbconnect";
 import { register } from "../model/register";
+import multer from "multer";
 import mysql from "mysql";
 
 
 
 export const router = express.Router();
-router.get("/:email/:password", (req, res) => {
-    let email = req.params.email;
-    let password = req.params.password;
-    const sql ="SELECT * FROM users WHERE email = ? AND password = ?";
-    conn.query(sql, [email, password], (err, result, fields) => {
+
+router.get("/", (req, res) => {
+    let typeuser = 'users';
+    const sql ="SELECT * FROM users WHERE types = ?";
+    conn.query(sql, [typeuser], (err, result, fields) => {
             // ตรวจสอบว่ามีผลลัพธ์หรือไม่
             if (result && result.length > 0) {
                 // ส่ง response กลับด้วยข้อมูลผู้ใช้
@@ -22,19 +23,40 @@ router.get("/:email/:password", (req, res) => {
                 });
             }
     });
+  });
+
+  
+router.get("/:email/:password", (req, res) => {
+    let email = req.params.email;
+    let password = req.params.password;
+    const sql = "SELECT * FROM users WHERE email = ? AND password = ?";
+    conn.query(sql, [email, password], (err, result, fields) => {
+        // ตรวจสอบว่ามีผลลัพธ์หรือไม่
+        if (result && result.length > 0) {
+            // ส่ง response กลับด้วยข้อมูลผู้ใช้
+            res.json(result);
+        } else {
+            // ถ้าไม่พบผู้ใช้, ส่ง response กลับเป็น { success: false }
+            res.json({
+                success: false,
+            });
+        }
+    });
 });
 
 router.post("/register",(req,res)=>{
     let register : register = req.body;
-    let sql = "INSERT INTO users(email, name,password,types) VALUES (?,?,?,?)";
+    let sql = "INSERT INTO users(email, name,password,types,avatar) VALUES (?,?,?,?,?)";
     sql = mysql.format(sql,[
         register.email,
         register.name,
         register.password,
         register.types,
+        register.avatar
     ]);
     conn.query(sql,(err,result)=>{
         if(err)throw err;
         res.status(201).json({affected_row: result.affectedRows });
     });
 });
+
